56745b8d3d356c2a8610b99603f4b5101cdaba82
sudo.park <sudo.park@kakao.com>
2026-05-14 23:00:39 +0900

    [#171] feat(domain): OAuth consent 모델 + 에러 타입 추가

---
+++ src/domain/errors/OAuthConsentError.ts
@@ -0,0 +1,16 @@
+export class InvalidChallengeError extends Error {
+  readonly code = 'invalid_challenge'
+  constructor() { super('Invalid or expired challenge') }
+}
+
+export class OAuthAsTransientError extends Error {
+  readonly code = 'as_transient'
+  readonly status?: number
+  readonly cause?: unknown
+
+  constructor(status?: number, cause?: unknown) {
+    super(`AS transient error${status ? ` (${status})` : ''}`)
+    this.status = status
+    this.cause = cause
+  }
+}
